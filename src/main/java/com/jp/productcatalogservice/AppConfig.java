7cf2145b3ce03e50017bf0479d806cf13ab824ce
jaiminpatel7024 <jaiminpatel7024@yahoo.com>
2025-03-27 12:35:05 +0530

    Integrated Eureka Discovery

--- src/main/java/com/jp/productcatalogservice/AppConfig.java
+++ src/main/java/com/jp/productcatalogservice/AppConfig.java
@@ -1,5 +1,8 @@
 package com.jp.productcatalogservice;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,34 +10,24 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
-
-    @Bean(name = "auth-service-validate")
-    public WebClient webClientAuthService(WebClient.Builder webClientBuilder)
-    {
-        return webClientBuilder
-                .baseUrl("http://localhost:8085/api/v1/validate")
-                .filter(new LoggingWebClientFilter())
-                .build();
-    }
+    @Autowired
+    EurekaClient eurekaClient;
 
     @Bean(name = "customer-service-web-client")
     public WebClient webClientCustomerService(WebClient.Builder webClientBuilder)
     {
+        InstanceInfo service = eurekaClient
+                .getApplication("ecom-customer-service")
+                .getInstances()
+                .get(0);
+
+        String hostName = service.getHostName();
+        int port = service.getPort();
+
         return webClientBuilder
-                .baseUrl("http://localhost:8101/api/v1")
+                .baseUrl("http://"+hostName+":"+port+"/api/v1")
                 .filter(new LoggingWebClientFilter())
                 .build();
     }
-
-    @Bean(name = "plain-old-web-client")
-    public WebClient webClientSubService(WebClient.Builder webClientBuilder)
-    {
-        return webClientBuilder
-                .baseUrl("http://localhost:8101/api/v1")
-                .filter(new LoggingWebClientFilter())
-                .build();
-    }
-
-
 
 }
