73e58d234b502ac53d956b7631bfad0f23d33faf
jaiminpatel7024 <jaiminpatel7024@yahoo.com>
2025-03-27 11:51:21 +0530

    Enabled Eureka Server

--- src/main/java/com/jp/productcatalogservice/ProductCatalogServiceApplication.java
+++ src/main/java/com/jp/productcatalogservice/ProductCatalogServiceApplication.java
@@ -2,7 +2,9 @@ package com.jp.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductCatalogServiceApplication {
 
